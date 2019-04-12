1137c5073b756974ab61039546135615bbd0351a
Devin Price <devingprice@gmail.com>
2019-04-12 19:33:00 -0400

    Add basic layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,6 +24,8 @@ var opponentHand = null;
 var playerId = null;
 var gameId = null;
 
+var chat = [];
+
 var $playerNumber = $('#player-number');
 var $playerId = $('#player-id');
 var $opponentId = $('#opponent-id');
@@ -33,8 +35,11 @@ var $opponentChoice = $('#opponent-choice');
 var $wins = $('#wins');
 var $losses = $('#losses');
 var $ties = $('#ties');
+var $chatlog = $('#chat-log');
+//var $chatButton = $('#chat-submit');
 
 function clearBoard() {
+
     //if i don't timeout the user chose from key click is updated before printing
     setTimeout(function () {
         var oldChoice = userHand;
@@ -45,143 +50,203 @@ function clearBoard() {
         $userChoice.text('You have yet to choose');
         $opponentChoice.text("Opponent is chosing");
 
-        $('#old-opponent-choice').html("Opponent's last choice: " + handChoiceToIcon(oldOppChoice) )
-        $('#old-user-choice').html("Players's last choice: " + handChoiceToIcon(oldChoice) )
+        $('#old-opponent-choice').html("Opponent's last choice: " + handChoiceToIcon(oldOppChoice))
+        $('#old-user-choice').html("Players's last choice: " + handChoiceToIcon(oldChoice))
 
     }, 1000)
 }
-
-db.ref('/game').on("value", function (snapshot) {
-    var values = snapshot.val();
-    console.log(values);
-
-    if (values !== null && values[playerId] && Object.keys(values).length > 1) {
-
-        var chosen = Object.keys(values);
-        var filtered = chosen.filter(function (value) {
-            return value !== playerId;
+function clearGameMoves(){
+    db.ref('/games').child(gameId).child('moves').set({})
+}
+function showResults(resultsType){
+    if(resultsType === 'win'){
+        console.log('win')
+        Swal.fire({
+            type: 'success',
+            title: 'YOU WIN!!!',
+            text: 'Great job!',
+            footer: createScoreDisplay()
         })
-        var opponentId = filtered[0];
-        $opponentId.text('opponenet ' + opponentId)
-        opponentHand = values[opponentId].choice;
-        //$opponentChoice.text('Your opponent choose: ' + opponentHand)
-        $opponentChoice.html( handChoiceToIcon(opponentHand) )
+    } else if( resultsType === 'lose'){
+        console.log('lose')
+        Swal.fire({
+            type: 'error',
+            title: 'You lose...',
+            text: 'nice...',
+            footer: createScoreDisplay()
+        })
+    } else if ( resultsType === 'tie'){
+        console.log('tie')
+        Swal.fire({
+            title: 'Tie',
+            text: '',
+            footer: createScoreDisplay()
+        })
+    }
+}
+function createScoreDisplay(){
+    return "<div id='wins'>Wins: "+ wins + "</div>"+"<div id='ties'>Ties: "+ ties + "</div>"+"<div id='losses'>Losses: "+ losses + "</div>";
+}
 
-        if ((userHand === "r" && opponentHand === "s") ||
-            (userHand === "s" && opponentHand === "p") ||
-            (userHand === "p" && opponentHand === "r")) {
-            console.log('win')
-            wins++;
-            db.ref('/game').set({})
-            clearBoard()
-            gameToResults()
-            Swal.fire({
-                type: 'success',
-                title: 'You win!!!',
-                text: 'Great job!'
+function createGameListeners() {
+    db.ref('/games').child(gameId).child('moves').on("value", function (snapshot) {
+        var values = snapshot.val();
+        console.log(values);
+
+        if (values !== null && values[playerId] && Object.keys(values).length > 1) {
+
+            var chosen = Object.keys(values);
+            var filtered = chosen.filter(function (value) {
+                return value !== playerId;
             })
-        } else if (userHand === opponentHand) {
-            console.log('tie')
-            ties++;
-            db.ref('/game').set({})
-            clearBoard()
-            gameToResults()
+            var opponentId = filtered[0];
+            $opponentId.text('opponenet ' + opponentId)
+            opponentHand = values[opponentId].choice;
             
-        } else {
-            console.log('lose')
-            losses++;
-            db.ref('/game').set({})
-            clearBoard()
-            gameToResults()
-            Swal.fire({
-                title: 'Tie',
-                text: 'Something went wrong!'
-            })
+            $opponentChoice.html(handChoiceToIcon(opponentHand))
+
+            if ((userHand === "r" && opponentHand === "s") ||
+                (userHand === "s" && opponentHand === "p") ||
+                (userHand === "p" && opponentHand === "r")) {
+                
+                wins++;
+                showResults('win');
+                clearGameMoves();
+                clearBoard()
+
+            } else if (userHand === opponentHand) {
+                
+                ties++;
+                showResults('tie');
+                clearGameMoves();
+                clearBoard()
+
+            } else {
+
+                losses++;
+                showResults('lose');
+                clearGameMoves();
+                clearBoard()
+
+            }
+
+            //$wins.text(wins);
+            //$losses.text(losses);
+            //$ties.text(ties);
+
         }
 
-        $wins.text(wins);
-        $losses.text(losses);
-        $ties.text(ties);
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
 
-    }
 
-}, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
-});
+    db.ref('/games').child(gameId).child('chat').on("child_added", function (snapshot, prevChildKey) {
+        var value = snapshot.val();
+        console.log( value );
+        chat.push( value.message )
+
+        $chatlog.empty();
+        for(var i =0; i< chat.length; i++){
+            $chatlog.append( '<p>'+ chat[i] + '</p>')
+        }
+    })
+}
 
 
 
 db.ref(".info/connected").on("value", function (snap) {
     var isConnected = snap.val();
-    if ( isConnected ) {
+    if (isConnected) {
         var con = db.ref("/connections").push(true);
         playerId = con.getKey();
-        console.log(playerId);
-        $playerId.text('player ' + playerId)
+        $playerId.text('playerId: ' + playerId)
         con.onDisconnect().remove();
 
         if (gameId === null) {
-            db.ref("/games").once('value', function (gamesObj) { //).then(
+            db.ref("/games").once('value', function (gamesObj) { 
                 gamesObj.forEach(function (gameObj) {
                     var gameKey = gameObj.key;
                     var playersList = gameObj.child('players').val();
-                    console.log('players list', playersList)
-                    if ( Object.keys(playersList).length < 2) {
-                        console.log(gameObj.child('players'))
+                    
+                    if (Object.keys(playersList).length < 2) {
                         db.ref('/games').child(gameKey).child('players').push(playerId)
                         gameId = gameKey;
-                        console.log('put in game ', gameKey)
                         return true;
                     }
                 })
-                if (gameId === null) {
+                if (gameId === null) { //create game
                     var gameObj = db.ref("/games").push(true);
                     gameObj.child('players').push(playerId)
                     gameId = gameObj.key;
-                    
+
                     gameObj.onDisconnect().remove();
-                    console.log('created game ', gameId)
                 }
+                $gameId.text('GameId: '+ gameId )
+                createGameListeners();
             })
         }
-        /*
-        gameslist for each
-            if game playerslist < 2, add player, return gameId
-            if no gameId, make new game, add player
-        */
-
-        // var gameObj = db.ref("/games").push(playerId);
-        // var gameId = gameObj.getKey();
-        // gameObj.onDisconnect().remove();
     }
 });
 
 db.ref("/connections").on("value", function (snapshot) {
     playerNumber = Object.keys(snapshot.val()).indexOf(playerId) + 1;
-    console.log(snapshot.val())
-    console.log(playerNumber)
     $playerNumber.text('You are player: ' + playerNumber)
 });
 
 
+/*
+/////Cant chat if this is constantly 'stealing' key presses
 document.onkeyup = function (event) {
     if (!userHand) {
         var userGuess = event.key;
-        pickPlayerChoice( userGuess )
+        pickPlayerChoice(userGuess)
     } else {
         alert('already played, wait on player 2')
     }
 }
-$(document).on('click', '.click-choice', function(){
+*/
+$(document).on('click', '.click-choice', function () {
     var userGuess = $(this).attr('data-click-val');
-    pickPlayerChoice( userGuess )
+    pickPlayerChoice(userGuess)
+})
+$(document).on('click', '#chat-submit', function () {
+    var input = $('#chat-input').val().trim();
+    $('#chat-input').val('')
+    if( gameId ){
+        db.ref('/games').child(gameId).child('chat').push({
+            message: input
+        })
+    } else {
+        Swal.fire({
+            type: 'warning',
+            title: 'Not in game',
+            text: 'You have no opponent to send a message to...'
+        })
+    }
+    
 })
 
-function pickPlayerChoice( userGuess ){
+var chatShowing = false;
+$(document).on('click', '#chat-expand', function () {
+    if( chatShowing ){
+        $('#chat-window').attr('style', "width: 0")
+        chatShowing = false;
+    } else {
+        $('#chat-window').attr('style', "width: 300px")
+        chatShowing = true;
+    }
+    
+})
+
+function pickPlayerChoice(userGuess) {
     if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
         userHand = userGuess;
 
-        db.ref('/game').child(playerId).set({
+        /*db.ref('/game').child(playerId).set({
+            choice: userHand
+        })*/
+        db.ref('/games').child(gameId).child('moves').child(playerId).set({
             choice: userHand
         })
 
@@ -190,9 +255,9 @@ function pickPlayerChoice( userGuess ){
     }
 }
 
-function handChoiceToIcon(handChoiceString, color){
+function handChoiceToIcon(handChoiceString, color) {
     var html = '';
-    if (handChoiceString === "r"){
+    if (handChoiceString === "r") {
         html = '<i class="fas fa-hand-rock fa-10x"></i>'
     } else if (handChoiceString === "p") {
         html = '<i class="fas fa-hand-paper fa-10x"></i>'
@@ -201,12 +266,14 @@ function handChoiceToIcon(handChoiceString, color){
     }
     return html;
 }
-function startToGame(){
+
+/*
+function startToGame() {
     setTimeout(function () {
         $('#start-screen').removeClass('screen--active');
         $('#game-screen').addClass('screen--active');
     }, 1000)
-    
+
 }
 function gameToResults() {
 
@@ -219,10 +286,10 @@ function gameToResults() {
         }, 1000)
     }, 1000)
 
-    
+
 }
 
-startToGame();
+startToGame();*/
 
 
 /*
